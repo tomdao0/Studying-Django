03a36f606a2acd4db806418b6e8c4d2d1abb6992
tom dao <tomdao1995@gmail.com>
2024-08-05 08:59:34 +0700

    update admin-site on 08 05 24

--- ETL_CCH/CCHManage/admin.py
+++ ETL_CCH/CCHManage/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import CLIENT, CLIENTCRS, STAFF
+
+
+class CLIENTADMIN(admin.ModelAdmin):
+    list_display = [
+        "ClientIdSubId",
+        "ClientSortName",
+        "ClientStatus",
+        "Created_Date",
+        # "ETL_CCH__FirmClientStaffAssignmentName",
+    ]
+    list_filter = ["ClientStatus"]
+    search_fields = ["ClientIdSubId", "ClientSortName"]
+
+
+class CLIENTCRSADMIN(admin.ModelAdmin):
+    list_display = [
+        "ClientIdSubId",
+        "StaffId",
+        "FirmClientStaffAssignmentName",
+    ]
+
 
 # Register your models here.
+admin.site.register(CLIENT, CLIENTADMIN)
+admin.site.register(CLIENTCRS, CLIENTCRSADMIN)
+admin.site.register(STAFF)
